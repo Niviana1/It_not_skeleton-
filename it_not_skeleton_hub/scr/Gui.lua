9215e7ccc82c3eb98754f1e456510325e7da6e22
Niv <145040402+Niviana1@users.noreply.github.com>
2024-02-29 21:15:30 +0700

    Update Gui.lua

--- it_not_skeleton_hub/scr/Gui.lua
+++ it_not_skeleton_hub/scr/Gui.lua
@@ -1,1 +1,21 @@
---[no gui yet]
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Prison Life Script", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local function addButton(tab, text, url)
+    tab:AddButton({
+        Name = text,
+        Callback = function()
+            loadstring(game:HttpGet(url, true))()
+        end
+    })
+end
+
+local PrisonLifeTab = Window:MakeTab({Name = "Prison Life", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+
+addButton(PrisonLifeTab, "Tiger Admin", "https://raw.githubusercontent.com/H17S32/Tiger_Admin/main/Script")
+addButton(PrisonLifeTab, "Prison Life Admin", "https://pastebin.com/raw/1RvgFtcx")
+addButton(PrisonLifeTab, "Super Punch", "https://pastebin.com/raw/KeCtc8B5")
+addButton(PrisonLifeTab, "Prison Life Tool Giver", "https://pastebin.com/raw/A7timJWF")
+addButton(PrisonLifeTab, "Empty", "")
+
+OrionLib:Init()
