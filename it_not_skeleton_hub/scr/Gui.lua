0e42bb720e20fb4c4bcf468b700949567d0fb2d1
Niv <145040402+Niviana1@users.noreply.github.com>
2024-03-02 04:56:06 +0700

    Update Gui.lua

--- it_not_skeleton_hub/scr/Gui.lua
+++ it_not_skeleton_hub/scr/Gui.lua
@@ -20,10 +20,34 @@ for i, buttonName in ipairs(mainButtons) do
     })
 end
 
+-- Admin Tab
+local adminTab = Window:MakeTab({Name = "Admin", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+
+local adminButtons = {
+    {"Infinite Yield", "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"},
+    {"CMD-X", "https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source"},
+    {"Fates", "https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"},
+    {"Proton", "https://api.luarmor.net/files/v3/loaders/5e6e6cc1bb32fd926764d064e2c60a3b.lua"},
+    {"Nameless Admin", "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"},
+    {"Legs Admin", "https://raw.githubusercontent.com/leg1337/legadmv2/main/legadminv2.lua"},
+    {"Homebrew", "https://raw.githubusercontent.com/Syntaxx64/HomebrewAdmin/master/Main"}
+}
+
+for _, data in ipairs(adminButtons) do
+    local buttonName = data[1]
+    local scriptUrl = data[2]
+    adminTab:AddButton({
+        Name = buttonName,
+        Callback = function()
+            loadstring(game:HttpGet(scriptUrl, true))()
+        end
+    })
+end
+
 -- Prison Life Tab
 local prisonLifeTab = Window:MakeTab({Name = "Prison Life", Icon = "rbxassetid://4483345998", PremiumOnly = false})
-
-local prisonLifeButtons = {"Tiger admin", "unknown admin?", "super punch", "tool giver"}
+prisonLifeTab:AddLabel("Disclaimer: I don't know what these scripts do. The names of the buttons are placeholders. Thank you to the script providers.")
+local prisonLifeButtons = {"Tiger Admin 2", "Prison Life Admin 3", "Super Punch 4", "Tool Giver"}
 local prisonLifeScriptUrls = {
     'https://raw.githubusercontent.com/H17S32/Tiger_Admin/main/TigeradminREVAMP.txt',
     'https://pastebin.com/raw/1RvgFtcx',
