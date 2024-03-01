d97e1fc41aa42aa82b28a467d49ae3aa15f31896
Niv <145040402+Niviana1@users.noreply.github.com>
2024-03-01 10:15:45 +0700

    Update Gui.lua

--- it_not_skeleton_hub/scr/Gui.lua
+++ it_not_skeleton_hub/scr/Gui.lua
@@ -1,20 +1,23 @@
 local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
-local Window = OrionLib:MakeWindow({Name = "It not skeletons gui V1", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Main", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local function addButton(tab, text, url)
-    tab:AddButton({
-        Name = text,
+local mainTab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+
+local buttons = {"Button 1", "Button 2", "Button 3"}
+
+local scriptUrls = {
+    'https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/HitboxExpander.lua',
+    'https://raw.githubusercontent.com/PABLOSleuth2/Scripts1/main/RobloxScripts/TEST/aimbot_v2.lua',
+    'https://raw.githubusercontent.com/Niviana1/Personal-exploit-/main/Mobilefly.lua'
+}
+
+for i, buttonName in ipairs(buttons) do
+    mainTab:AddButton({
+        Name = buttonName,
         Callback = function()
-            loadstring(game:HttpGet(url))()
+            loadstring(game:HttpGet(scriptUrls[i]))()
         end
     })
 end
-
-local PrisonLifeTab = Window:MakeTab({Name = "Prison Life", Icon = "rbxassetid://4483345998", PremiumOnly = false})
-
-addButton(PrisonLifeTab, "Tiger Admin", "https://raw.githubusercontent.com/H17S32/Tiger_Admin/main/Script")
-addButton(PrisonLifeTab, "Prison Life Admin", "https://pastebin.com/raw/1RvgFtcx")
-addButton(PrisonLifeTab, "Super Punch", "https://pastebin.com/raw/KeCtc8B5")
-addButton(PrisonLifeTab, "Prison Life Tool Giver", "https://pastebin.com/raw/A7timJWF")
 
 OrionLib:Init()
