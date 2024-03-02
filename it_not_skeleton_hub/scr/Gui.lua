3c6b794fc2f655c8bf9ffa85268689657fa61520
Niv <145040402+Niviana1@users.noreply.github.com>
2024-03-02 21:10:42 +0700

    Update Gui.lua

--- it_not_skeleton_hub/scr/Gui.lua
+++ it_not_skeleton_hub/scr/Gui.lua
@@ -1,72 +1,61 @@
 local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
-local Window = OrionLib:MakeWindow({Name = " it's not skeleton hub v0.6 w.i.p ", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "I.N.S", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+-- Function to create buttons for a tab
+local function createButtons(tab, buttons)
+    for _, data in ipairs(buttons) do
+        local buttonName = data[1]
+        local scriptUrl = data[2]
+        tab:AddButton({
+            Name = buttonName,
+            Callback = function()
+                loadstring(game:HttpGet(scriptUrl, true))()
+            end
+        })
+    end
+end
 
 -- Main Tab
-local mainTab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998", PremiumOnly = false})
-
-local mainButtons = {"Expander", "Aimbot", "Mobile Fly", "ESP", "Mobile Keyboard", "Teleport Tool"}
-local mainScriptUrls = {
-    'https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/HitboxExpander.lua',
-    'https://raw.githubusercontent.com/PABLOSleuth2/Scripts1/main/RobloxScripts/TEST/aimbot_v2.lua',
-    'https://raw.githubusercontent.com/Niviana1/Personal-exploit-/main/Mobilefly.lua',
-    'https://raw.githubusercontent.com/Niviana1/It_not_skeleton-/main/Scripts/Esp.lua',
-    'https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt',
-    'https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Teleport%20Tool/Source.lua'
+local mainTab = Window:MakeTab({Name = "🔧|Main", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local mainButtons = {
+    {"Expander", 'https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/HitboxExpander.lua'},
+    {"Aimbot", 'https://raw.githubusercontent.com/PABLOSleuth2/Scripts1/main/RobloxScripts/TEST/aimbot_v2.lua'},
+    {"Mobile Fly", 'https://raw.githubusercontent.com/Niviana1/Personal-exploit-/main/Mobilefly.lua'},
+    {"ESP", 'https://raw.githubusercontent.com/Niviana1/It_not_skeleton-/main/Scripts/Esp.lua'},
+    {"Mobile Keyboard", 'https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt'},
+    {"Teleport Tool", 'https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Teleport%20Tool/Source.lua'}
 }
-
-for i, buttonName in ipairs(mainButtons) do
-    mainTab:AddButton({
-        Name = buttonName,
-        Callback = function()
-            loadstring(game:HttpGet(mainScriptUrls[i]))()
-        end
-    })
-end
+createButtons(mainTab, mainButtons)
 
 -- Admin Tab
-local adminTab = Window:MakeTab({Name = "Admin", Icon = "rbxassetid://4483345998", PremiumOnly = false})
-
+local adminTab = Window:MakeTab({Name = "🛠️|Admin", Icon = "rbxassetid://4483345998", PremiumOnly = false})
 local adminButtons = {
-    {"Infinite Yield", "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"},
-    {"CMD-X", "https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source"},
-    {"Fates", "https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"},
-    {"Proton", "https://api.luarmor.net/files/v3/loaders/5e6e6cc1bb32fd926764d064e2c60a3b.lua"},
-    {"Nameless Admin", "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"},
-    {"Legs Admin", "https://raw.githubusercontent.com/leg1337/legadmv2/main/legadminv2.lua"},
-    {"Homebrew", "https://raw.githubusercontent.com/Syntaxx64/HomebrewAdmin/master/Main"}
+    {"Infinite Yield", 'https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'},
+    {"CMD-X", 'https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source'},
+    {"Fates", 'https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua'},
+    {"Proton", 'https://api.luarmor.net/files/v3/loaders/5e6e6cc1bb32fd926764d064e2c60a3b.lua'},
+    {"Nameless Admin", 'https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source'},
+    {"Legs Admin", 'https://raw.githubusercontent.com/leg1337/legadmv2/main/legadminv2.lua'},
+    {"Homebrew", 'https://raw.githubusercontent.com/Syntaxx64/HomebrewAdmin/master/Main'}
 }
-
-for _, data in ipairs(adminButtons) do
-    local buttonName = data[1]
-    local scriptUrl = data[2]
-    adminTab:AddButton({
-        Name = buttonName,
-        Callback = function()
-            loadstring(game:HttpGet(scriptUrl, true))()
-        end
-    })
-end
+createButtons(adminTab, adminButtons)
 
 -- Prison Life Tab
-local prisonLifeTab = Window:MakeTab({Name = "Prison Life", Icon = "rbxassetid://4483345998", PremiumOnly = false})
-local disclaimerSection = prisonLifeTab:AddSection({Name = "Disclaimer"})
-disclaimerSection:AddLabel("I don't know what these scripts do. The names of the buttons are placeholders. Thank you to the script providers.")
-
-local prisonLifeButtons = {"Tiger Admin", "Prison Life Admin", "Super Punch", "Tool Giver"}
-local prisonLifeScriptUrls = {
-    'https://raw.githubusercontent.com/H17S32/Tiger_Admin/main/TigeradminREVAMP.txt',
-    'https://pastebin.com/raw/1RvgFtcx',
-    'https://pastebin.com/raw/KeCtc8B5',
-    'https://pastebin.com/raw/A7timJWF'
+local prisonLifeTab = Window:MakeTab({Name = "🔒|Prison Life", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local prisonLifeButtons = {
+    {"Tiger Admin", 'https://raw.githubusercontent.com/H17S32/Tiger_Admin/main/TigeradminREVAMP.txt'},
+    {"Prison Life Admin", 'https://pastebin.com/raw/1RvgFtcx'},
+    {"Super Punch", 'https://pastebin.com/raw/KeCtc8B5'},
+    {"Tool Giver", 'https://pastebin.com/raw/A7timJWF'}
 }
+createButtons(prisonLifeTab, prisonLifeButtons)
 
-for i, buttonName in ipairs(prisonLifeButtons) do
-    prisonLifeTab:AddButton({
-        Name = buttonName,
-        Callback = function()
-            loadstring(game:HttpGet(prisonLifeScriptUrls[i]))()
-        end
-    })
-end
+-- Thy Hood Tab
+local thyHoodTab = Window:MakeTab({Name = "🇬🇧|Thy Hood", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local thyHoodButtons = {
+    {"Print People's Money", 'https://raw.githubusercontent.com/Niviana1/It_not_skeleton-/main/Scripts/Print_people_money_to_console.lua'},
+    {"Get Fake Holy Sword", 'https://raw.githubusercontent.com/Niviana1/It_not_skeleton-/main/Scripts/Get_thefakeholyswordthingy.lua'}
+}
+createButtons(thyHoodTab, thyHoodButtons)
 
 OrionLib:Init()
